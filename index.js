e47eb2773edd0b767caa2a658f7546060a7ebf7c
user <user@users-MacBook-Air.local>
2023-01-21 22:09:03 +0600

    first commit

---
+++ index.js
@@ -0,0 +1,78 @@
+let player = {
+  firstName : "Shams",
+  money : 0
+};
+
+let messageEl = document.getElementById("message-el");
+let sumEl = document.getElementById("sum-el");
+let cardEl = document.getElementById("card-el");
+let playerEl = document.getElementById("player-el");
+
+let cards = [];
+let sum = 0;
+let message = "";
+let isAlive = false;
+let hasBlackjack = false;
+
+function getRandomCard() {
+  return Math.floor(Math.random() * 10) + 2;
+}
+
+function renderScore() {
+  playerEl.innerText = player.firstName + ": $" + player.money;
+}
+
+function renderGame() {
+  
+  if(sum < 21){
+    message = "Wanna draw a new card?";
+  }
+  else if(sum === 21){
+    message = "Congratulations! You've got BLACKJACK!";
+    hasBlackjack = true;
+    player.money = 100;
+    renderScore();
+  }
+  else{
+    message = "You're out of the game!";
+    isAlive = false;
+    renderScore();
+  }
+  
+  messageEl.innerText = message;
+  sumEl.innerText = "Sum : " + sum;
+  cardEl.innerText = "Cards :";
+  
+  for(let i = 0; i < cards.length; i++) {
+    cardEl.innerText += " " + cards[i];
+  }
+}
+
+function newCard() {
+
+  if(isAlive === false || hasBlackjack === true){
+    return;
+  }
+
+  let card = getRandomCard();
+  
+  sum += card;
+  cards.push(card);
+  renderGame();
+}
+
+function startGame() {
+  
+  let card1 = getRandomCard();
+  let card2 = getRandomCard();
+  
+  cards = [card1, card2];
+  sum = card1 + card2;
+  isAlive = true;
+  hasBlackjack = false;
+  player.money = 0
+
+  playerEl.innerText = "";
+  
+  renderGame();
+}
